446bba892e86a331f93477a4228995208a8ac4f8
kama-jo <couturier-jonathan@hotmail.fr>
2021-04-15 12:19:02 +0200

    Update client.js

--- client/client.js
+++ client/client.js
@@ -1,12 +1,17 @@
 // import <script src="https://cdn.socket.io/3.1.3/socket.io.min.js"></script>
 
+const identity = {
+    session: 123456789,
+    id: 1,
+    service: "test",
+    onglet: "onglet1"
+}
+
 const socket = io('http://localhost:3000')
 socket.on('connect', () => {
-    socket.emit('identify', {
-        session: 123456789,
-        id: 1,
-        service: "test",
-        onglet: "onglet1"
-    })
-    console.log("Connecté")
+    socket.emit('identify', identity)
+})
+
+socket.on('onglet', ({ onglet }) => {
+    console.log(onglet)
 })
